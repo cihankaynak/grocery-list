76a78ec8b9a2afa20e0f5a996237d0fc81264ab3
Cihan Kaynak <cihan.kaynak@cerner.com>
2021-04-02 14:41:04 -0500

    Fixed checkbox: checked when completed. Otherwise not checked.

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,11 @@ import { FaTrash } from "react-icons/fa";
 const Item = ({ item, onItem, onDelete }) => {
   return (
     <article className="grocery-item">
-      <input type="checkbox" onClick={() => onItem(item.id)} />
+      <input
+        type="checkbox"
+        onClick={() => onItem(item.id)}
+        checked={item.isCompleted}
+      />
       <p className={item.isCompleted ? "completed" : ""}>{item.name}</p>
       <button className="delete-btn" onClick={() => onDelete(item.id)}>
         <FaTrash />
